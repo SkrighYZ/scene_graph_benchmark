d0f5bd65c0292c83e46f482e6e4a836997f0270d
SkrighYZ <zhang.yipeng@columbia.edu>
2021-11-04 17:24:42 -0400

    .

--- maskrcnn_benchmark/engine/inference.py
+++ maskrcnn_benchmark/engine/inference.py
@@ -164,9 +164,6 @@ def convert_predictions_to_tsv(predictions, dataset, output_folder,
                         cur_d['rect'] = boxes[i]
                         cur_d['bbox_id'] = i
                     if name == 'class':
-                        print(labels)
-                        print(labels[i])
-                        print(labelmap)
                         cur_d['class'] = labelmap[labels[i]]
                     if name == 'conf':
                         cur_d['conf'] = scores[i]
