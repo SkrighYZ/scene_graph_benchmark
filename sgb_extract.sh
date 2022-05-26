32bd6c99cb18b7d2e8841eb8f9461b21b983cd46
SkrighYZ <zhang.yipeng@columbia.edu>
2022-05-26 19:14:06 -0400

    add ood

--- sgb_extract.sh
+++ sgb_extract.sh
@@ -1,1 +1,1 @@
-python tools/test_sg_net.py --config-file sgg_configs/vgattr/vinvl_x152c4.yaml TEST.IMS_PER_BATCH 8 MODEL.ROI_HEADS.NMS_FILTER 1 MODEL.ROI_HEADS.SCORE_THRESH 0.2 TEST.IGNORE_BOX_REGRESSION True MODEL.ATTRIBUTE_ON True
+python tools/test_sg_net.py --config-file sgg_configs/vgattr/vinvl_x152c4.yaml TEST.IMS_PER_BATCH 1 MODEL.ROI_HEADS.NMS_FILTER 1 MODEL.ROI_HEADS.SCORE_THRESH 0.2 TEST.IGNORE_BOX_REGRESSION True MODEL.ATTRIBUTE_ON True
